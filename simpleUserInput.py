a27fd5a2d6b5de750ebcbc5bbf63b593ce716233
sevenwheels55 <nathanseto@gmail.com>
2016-04-18 14:59:00 -0700

    new files

--- simpleUserInput.py
+++ simpleUserInput.py
@@ -1,10 +1,9 @@
-something = input("User inputs a string data: ")
+something =  input("Enter some input: ")
 
 print("The unadulterated input: " + something)
-
+print("As an integer: ")
+print(int(something))
 print("As a float: ")
 print(float(something))
 print("As a boolean value: ")
 print(bool(something))
-print("As an integer: ")
-print(int(something))
