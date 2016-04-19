85dee0569dd78f64d53aa9615c3b67aef52c9990
sevenwheels55 <nathanseto@gmail.com>
2016-04-18 17:07:50 -0700

    Added randomGeneration.py

--- simpleUserInput.py
+++ simpleUserInput.py
@@ -1,9 +1,8 @@
 something =  input("Enter some input: ")
 
-print("The unadulterated input: " + something)
-print("As an integer: ")
-print(int(something))
-print("As a float: ")
-print(float(something))
-print("As a boolean value: ")
-print(bool(something))
+print("The unadulterated input: ", something)
+print("As an integer: ", int(something))
+
+print("As a float: ", float(something))
+
+print("As a boolean value: ", bool(something))
