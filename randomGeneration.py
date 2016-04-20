fa8314d335d7778802157ccdadcfa3f0631bde9f
sevenwheels55 <nathanseto@gmail.com>
2016-04-19 20:21:30 -0700

    Added randomGeneration.py

---
+++ randomGeneration.py
@@ -0,0 +1,8 @@
+import random
+
+"""Define the upper and lower limits of the integer that you want to generate"""
+lowerInt = 0
+upperInt = 100
+num = random.randint(lowerInt, upperInt)
+
+print(num)
